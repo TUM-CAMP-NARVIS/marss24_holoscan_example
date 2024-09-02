f38805258f10da2a359ef168c59cf42f4f671203
Ulrich  Eck <ulrich.eck@tum.de>
2024-09-02 06:27:27 +0200

    basic example of opencv in hololens

---
+++ run_container.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#export NGC_CONTAINER_IMAGE_PATH="nvcr.io/nvidia/clara-holoscan/holoscan:v2.3.0-dgpu"
+export NGC_CONTAINER_IMAGE_PATH="holoscan-custom"
+
+xhost +local:docker
+
+docker run -it --rm --net host \
+  --runtime=nvidia \
+  --ipc=host --cap-add=CAP_SYS_PTRACE --ulimit memlock=-1 --ulimit stack=67108864 \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY \
+  --user 1000:1000 -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro \
+  ${NGC_CONTAINER_IMAGE_PATH}
+
